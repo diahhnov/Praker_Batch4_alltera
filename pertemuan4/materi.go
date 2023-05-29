4ffb89f0f819e26c0008977d6eb0f5d4f36e3dc3
Diah Novianti <diahnovianti0711@gmail.com>
2023-05-29 13:37:09 +0700

    update materi

--- pertemuan4/materi.go
+++ pertemuan4/materi.go
@@ -1,56 +1,17 @@
-// // package main
+// package main
 
-// // import "fmt"
+// import "fmt"
 
-// // func main() {
-// // 	//pointer
-// // 	var umur int = 20
-// // 	var name string = "alterra"
-// // 	// var alamat string = "jalan bintaro"
-// // 	var alamatUmur *int = &umur
-// // 	fmt.Println(&umur, &name, &alamatUmur)// 0xc00001a098 0xc000050270 0xc00000a028
-// // 	fmt.Println(alamatUmur, &alamatUmur)// 0xc00001a098 0xc00000a028
-// // }
+// func main() {
+// 	//pointer
+// 	var umur int = 20
+// 	var name string = "alterra"
+// 	// var alamat string = "jalan bintaro"
+// 	var alamatUmur *int = &umur
+// 	fmt.Println(&umur, &name, &alamatUmur)// 0xc00001a098 0xc000050270 0xc00000a028
+// 	fmt.Println(alamatUmur, &alamatUmur)// 0xc00001a098 0xc00000a028
+// }
 
-// // package main
-
-// // import "fmt"
-
-// // type People struct {
-// // 	name    string
-// // 	age     int
-// // 	address string
-// // 	health  int
-// // }
-
-// //metode
-// // func (people People) Jalan() int {
-// // 	return people.age * 2
-// // }
-
-// // func main() {
-// 	// var people People // people := People{}
-// 	// people.name = "Diah"
-// 	// people.age = 10
-// 	// people.address = "jaktim"
-// 	// people.Jalan() // memanggil metode
-// 	// people.health = Jalan(human) // memangil func
-// 	// fmt.Println(people.health)
-
-// 	// ubahData(people)
-// 	// fmt.Println(people.name, people.age, people.address) // memanggil
-// // }
-
-// // func ubahData(people People) {
-// // 	people.name = ""
-// // 	people.age = 1
-// // 	people.address = ""
-// // }
-
-// /*
-//  */
-
-// // Interface : jembatan antara request dengan server
 // package main
 
 // import "fmt"
@@ -62,37 +23,76 @@
 // 	health  int
 // }
 
-// type HumanInterface interface{
-// 	Lari()
-// 	Jalan()
-// }
-
-// func (people People) Lari() int {
-// 	people.health = people.age*2
-// 	return people.health
-// }
-
-// //metode
+//metode
 // func (people People) Jalan() int {
-// 	people.health = people.age*2
-// 	return people.health
+// 	return people.age * 2
 // }
 
 // func main() {
-// 	var people People // people := People{}
-// 	people.name = "Diah"
-// 	people.age = 10
-// 	people.address = "jaktim"
-// 	people.health = 17
-// 	people.Jalan() // memanggil metode
-// 	// people.health = Jalan(human) // memangil func
-// 	fmt.Println(people.health)
+// var people People // people := People{}
+// people.name = "Diah"
+// people.age = 10
+// people.address = "jaktim"
+// people.Jalan() // memanggil metode
+// people.health = Jalan(human) // memangil func
+// fmt.Println(people.health)
 
-// 	// ubahData(people)
-// 	// fmt.Println(people.name, people.age, people.address) // memanggil
-
-// 	var humanInterface HumanInterface
-// 	humanInterface = People
-// 	fmt.Println(humanInterface.Jalan())
-// 	fmt.Println(humanInterface.Lari())
+// ubahData(people)
+// fmt.Println(people.name, people.age, people.address) // memanggil
 // }
+
+// func ubahData(people People) {
+// 	people.name = ""
+// 	people.age = 1
+// 	people.address = ""
+// }
+
+/*
+ */
+
+// Interface : jembatan antara request dengan server
+package main
+
+import "fmt"
+
+type People struct {
+	name    string
+	age     int
+	address string
+	health  int
+}
+
+type HumanInterface interface {
+	Lari()
+	Jalan()
+}
+
+func (people *People) Lari() {
+	people.health = people.health * 2
+}
+
+//metode
+func (people *People) Jalan() {
+	people.health = people.health * 2
+}
+
+func main() {
+	people := People{}
+	people.name = "Diah"
+	people.age = 10
+	people.address = "jaktim"
+	people.health = 17
+	people.Jalan() // memanggil metode
+	// people.health = Jalan(human) // memangil func
+	fmt.Println(people.health)
+
+	// ubahData(people)
+	// fmt.Println(people.name, people.age, people.address) // memanggil
+
+	var humanInterface HumanInterface
+	humanInterface = &people
+	humanInterface.Lari()
+	humanInterface.Jalan()
+	fmt.Println(humanInterface.Jalan)
+	fmt.Println(humanInterface.Lari)
+}
